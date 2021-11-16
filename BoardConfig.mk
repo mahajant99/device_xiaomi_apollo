5567d859366108080e164931c121ad120d57f692
Mesquita <mesquita@aospa.co>
2021-11-16 17:11:00 +0800

    apollo: Drop fwk detect props
     * Already defined in pa build
    
    Signed-off-by: Mesquita <mesquita@aospa.co>
    Signed-off-by: Tushar Mahajan <mahajant99@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -125,7 +125,6 @@ BOARD_USES_QCOM_HARDWARE := true
 TARGET_TAP_TO_WAKE_NODE := "/sys/touchpanel/double_tap"
 
 # Properties
-TARGET_ODM_PROP += $(DEVICE_PATH)/odm.prop
 TARGET_PRODUCT_PROP += $(DEVICE_PATH)/product.prop
 TARGET_SYSTEM_PROP += $(DEVICE_PATH)/system.prop
 TARGET_VENDOR_PROP += $(DEVICE_PATH)/vendor.prop
