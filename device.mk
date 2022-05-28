f09d4bf0dd75a0fed9a69c19da18022d41851e57
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2022-05-28 10:27:37 +0200

    apollon: Drop atrace HIDL
    Change-Id: Iaaaf62e9dda26dd15d51f5975d9f21fe1037d2eb

--- device.mk
+++ device.mk
@@ -68,10 +68,6 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/com.android.nfc_extras.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/com.android.nfc_extras.xml \
     frameworks/native/data/etc/com.nxp.mifare.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/com.nxp.mifare.xml
 
-# Atrace
-PRODUCT_PACKAGES += \
-    android.hardware.atrace@1.0-service
-
 # Audio
 PRODUCT_PACKAGES += \
     android.hardware.audio@6.0-impl \
