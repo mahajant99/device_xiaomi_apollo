502e30b51616e34af3f022a06412ff59a95af064
Tushar Mahajan <mahajant99@gmail.com>
2022-07-07 22:46:47 +0530

    apollo: Add surfaceflinger refresh rate prop * from apollo aosp tree
    Signed-off-by: Tushar Mahajan <mahajant99@gmail.com>
    Change-Id: Ib3dada720317d6f0ef6ba27ffc93ebce0f50b03d

--- device.mk
+++ device.mk
@@ -150,6 +150,14 @@ PRODUCT_PROPERTY_OVERRIDES += \
     vendor.display.use_layer_ext=0 \
     vendor.display.use_smooth_motion=0
 
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.surface_flinger.use_content_detection_for_refresh_rate=true
+    ro.surface_flinger.set_display_power_timer_ms=1000
+    ro.surface_flinger.set_idle_timer_ms=80
+    ro.surface_flinger.set_touch_timer_ms=200
+    vendor.display.idle_time=1100
+    debug.sf.frame_rate_multiple_threshold=144
+
 PRODUCT_COPY_FILES += \
     $(call find-copy-subdir-files,*,$(LOCAL_PATH)/configs/display/,$(TARGET_COPY_OUT_VENDOR)/etc)
 
