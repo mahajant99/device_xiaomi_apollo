2d63239929efc471293363f84ba471689ac407f5
Gokul GK <gk@gkcloud.app>
2023-02-08 21:54:27 +0530

    apollo: Use common compatibility matrix
    Change-Id: I485371e3225fafff31646469a008c2d84efbf8eb
    Signed-off-by: Tushar Mahajan <mahajant99@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -77,9 +77,6 @@ DEVICE_MANIFEST_FILE += \
     $(DEVICE_PATH)/configs/vintf/manifest.xml \
     $(DEVICE_PATH)/configs/vintf/xiaomi_manifest.xml
 
-DEVICE_MATRIX_FILE += \
-    device/qcom/common/compatibility_matrix.xml
-
 # Init
 TARGET_INIT_VENDOR_LIB := //$(DEVICE_PATH):libinit_apollo
 TARGET_RECOVERY_DEVICE_MODULES := libinit_apollo
