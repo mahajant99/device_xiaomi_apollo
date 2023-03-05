c7531ea179980f269ed6cf8af14dd9ecd5350b01
Sebastiano Barezzi <seba@sebaubuntu.dev>
2023-03-05 01:07:30 +0100

    apollon: Build ANT+ HIDL interface library from source
    Change-Id: Ic6f14cc72a6fb8655b43ca89f7aa9cf977a0d795

--- device.mk
+++ device.mk
@@ -66,6 +66,10 @@ PRODUCT_COPY_FILES += \
 PRODUCT_AAPT_CONFIG := normal
 PRODUCT_AAPT_PREF_CONFIG := xxhdpi
 
+# ANT+
+PRODUCT_PACKAGES += \
+    com.dsi.ant@1.0.vendor
+
 # Audio
 PRODUCT_PACKAGES += \
     android.hardware.audio@6.0-impl \
