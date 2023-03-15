100290cacef7437557fa244407fff6900ee0b788
Sebastiano Barezzi <seba@sebaubuntu.dev>
2023-03-15 22:31:18 +0100

    apollon: Update blobs from munch V14.0.1.0.TLMMIXM
    Change-Id: Idc5b8ee2dbebae3cb93eb5c74d496a662b5d7ae5

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -149,7 +149,7 @@ TARGET_RELEASETOOLS_EXTENSIONS := $(DEVICE_PATH)
 ENABLE_VENDOR_RIL_SERVICE := true
 
 # Security patch level
-VENDOR_SECURITY_PATCH := 2022-08-01
+VENDOR_SECURITY_PATCH := 2022-12-01
 
 # Sepolicy
 include device/qcom/sepolicy_vndr-legacy-um/SEPolicy.mk
