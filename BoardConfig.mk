b232ff60db005a826e110313c1621753248b6538
althafvly <althafvly@gmail.com>
2023-06-08 14:23:41 +0530

    apollon: Update blobs from munch V14.0.2.0.TLMMIXM
    Change-Id: I00a9958e59fc9e9394db40b2ea894ecb19a29bc3

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -146,7 +146,7 @@ TARGET_RELEASETOOLS_EXTENSIONS := $(DEVICE_PATH)
 ENABLE_VENDOR_RIL_SERVICE := true
 
 # Security patch level
-VENDOR_SECURITY_PATCH := 2022-12-01
+VENDOR_SECURITY_PATCH := 2023-03-01
 
 # Sepolicy
 include device/qcom/sepolicy_vndr-legacy-um/SEPolicy.mk
