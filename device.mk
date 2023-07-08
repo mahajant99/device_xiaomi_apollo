b528af5ac47211ea36061a1fc6cef34e52614282
Sebastiano Barezzi <seba@sebaubuntu.dev>
2023-07-08 15:14:37 -0300

    apollo: Build Xiaomi sensors impl Signed-off-by: Tushar Mahajan <mahajant99@gmail.com>
    Change-Id: Ib477ac016c312ce02e9f71df2a7d1d0fcca717e8

--- device.mk
+++ device.mk
@@ -289,7 +289,7 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.sensor.stepdetector.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.sensor.stepdetector.xml
 
 PRODUCT_PACKAGES += \
-    android.hardware.sensors@1.0-impl \
+    android.hardware.sensors@1.0-impl-xiaomi \
     android.hardware.sensors@1.0-service \
     libsensorndkbridge
 
