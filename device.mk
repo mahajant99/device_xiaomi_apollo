dff21b294eaeb7aa15f0bf333a733839ab97dd2a
Tushar Mahajan <mahajant99@gmail.com>
2023-09-05 14:29:52 +0530

    apollo: Fix smooth display
    Change-Id: I03967dfbe7814a311bb8b383f6ae6820b0a7f127
    Signed-off-by: Tushar Mahajan <mahajant99@gmail.com>

--- device.mk
+++ device.mk
@@ -147,11 +147,11 @@ PRODUCT_VENDOR_PROPERTIES += \
     vendor.display.defer_fps_frame_count=2
 
 PRODUCT_DEFAULT_PROPERTY_OVERRIDES += \
-    ro.surface_flinger.use_content_detection_for_refresh_rate=true
-    ro.surface_flinger.set_display_power_timer_ms=1000
-    ro.surface_flinger.set_idle_timer_ms=1000
-    ro.surface_flinger.set_touch_timer_ms=100
-    vendor.display.idle_time=1100
+    ro.surface_flinger.use_content_detection_for_refresh_rate=true \
+    ro.surface_flinger.set_display_power_timer_ms=1000 \
+    ro.surface_flinger.set_idle_timer_ms=1000 \
+    ro.surface_flinger.set_touch_timer_ms=100 \
+    vendor.display.idle_time=1100 \
     debug.sf.frame_rate_multiple_threshold=144
 
 PRODUCT_COPY_FILES += \
